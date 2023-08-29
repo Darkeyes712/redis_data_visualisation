09792d96f18c9da5fe1d47359aa50d06f04f2c25
Nikola Kolev <nikolakolev712@gmail.com>
2023-08-29 13:13:01 +0300

    handle Result in main

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ mod redis;
 
 fn main() {
     let red = redis::RedisClass {};
-    red.convert_data_to_json();
+    match red.convert_data_to_json() {
+        Ok(result) => println!("{}", result),
+        Err(error) => println!("{}", error),
+    }
 }
